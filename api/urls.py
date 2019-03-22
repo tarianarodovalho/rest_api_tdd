0399a1d36d3a0bc025eedf9b15095a92c9e87342
tarianarodovalho <tariana.rodovalho@gmail.com>
2019-03-22 17:14:45 -0300

    Creating serializer Defining view urls

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-"""api URL Configuration
+"""rest_api_tdd URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path('api/(?P<version>(v1|v2))/', include('music.urls'))
 ]
